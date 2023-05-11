d9a56720c383e34222145b1b63b3b35f355b5f96
AlexVi <vpa24082012@gmal.com>
2023-05-11 22:54:14 +0300

    Добавили папку Task21 c решением задачи в трех вариантах

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -53,6 +53,6 @@ double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
 
 // Вариант 3
 {
-     double distance = Math.Sqrt (Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+     double distance Math.Sqrt (Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
      return distance;
 }
